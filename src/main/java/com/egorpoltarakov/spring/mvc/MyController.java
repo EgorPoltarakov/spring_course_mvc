f04efc8bb85123fda800b93c17673c91a582794b
Poltarakov <epoltarakov.99@mail.ru>
2022-07-11 17:48:24 +0300

    Create Model

--- src/main/java/com/egorpoltarakov/spring/mvc/MyController.java
+++ src/main/java/com/egorpoltarakov/spring/mvc/MyController.java
@@ -2,7 +2,10 @@ package com.egorpoltarakov.spring.mvc;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -18,8 +21,21 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(){
+//        return "show-emp-details-view";
+//    }
+
+
     @RequestMapping("/showDetails")
-    public String showEmpDetails(){
+    public String showEmpDetails(HttpServletRequest request, Model model){
+
+        String empName = request.getParameter("employeeName");
+        empName = "Mr. " + empName;
+        model.addAttribute("nameAttribute", empName);
+
+        model.addAttribute("description", " - student");
+
         return "show-emp-details-view";
     }
 }
